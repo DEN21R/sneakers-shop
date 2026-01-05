1e6194f1225872df03c552965ff8ae846e65cba7
DEN21R <denr@ukr.net>
2026-01-05 21:46:04 +0200

    added styles to Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,44 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Toolbar, Typography, Box } from '@mui/material'
+import { NavLink } from 'react-router-dom'
+
+const menuNav = [
+  {
+    path: '/',
+    title: 'Главная',
+  },
+  {
+    path: '/cart',
+    title: 'Корзина',
+  },
+  {
+    path: '/contacts',
+    title: 'Контакты',
+  },
+]
 
 function Header() {
   return (
-    <AppBar>
-      <Toolbar>
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: '#3B3C3D', px: 31.25, py: 5.4 }}
+    >
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Typography sx={{ fontSize: '20px', fontWeight: 900 }}>
           Сникер - магазин
         </Typography>
+        <Box sx={{ display: 'flex', gap: 8 }}>
+          {menuNav.map((el, index) => (
+            <NavLink
+              key={index}
+              to={el.path}
+              style={({ isActive }) => ({
+                color: isActive ? '#FFFFFF' : '#FFFFFF80',
+              })}
+            >
+              {el.title}
+            </NavLink>
+          ))}
+        </Box>
       </Toolbar>
     </AppBar>
   )
