6dfaa23609f36a9951f32057656442d32388ea8a
DEN21R <denr@ukr.net>
2026-01-07 12:18:11 +0200

    continued writing Main,added ProductCard

--- src/components/productCard/index.jsx
+++ src/components/productCard/index.jsx
@@ -1,6 +1,47 @@
-import { Box } from '@mui/material'
+import { CardContent, Card, Typography, CardMedia } from '@mui/material'
 
-function ProductCard() {
-  return <Box></Box>
+function ProductCard({ product }) {
+  return (
+    <Card
+      sx={{
+        maxWidth: 386,
+        width: '100%',
+        borderRadius: 5,
+        border: '2px solid #00000026',
+        p: 5,
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={product.image}
+        alt={product.title}
+        sx={{
+          height: 220,
+          objectFit: 'cover',
+        }}
+      />
+      <Typography
+        sx={{ fontSize: 24, fontWeight: 400, color: '#000000', mb: 5 }}
+      >
+        {product.name}
+      </Typography>
+
+      <CardContent
+        sx={{
+          p: 0,
+          '&:last-child': {
+            pb: 0,
+          },
+        }}
+      >
+        <Typography sx={{ fontSize: 14, fontWeight: 500, color: '#666666' }}>
+          Цена:
+        </Typography>
+        <Typography sx={{ fontSize: 24, fontWeight: 700, color: '#000000' }}>
+          {product.price} €
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 }
 export default ProductCard
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,16 +1,41 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Grid, Divider } from '@mui/material'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import News from '../../components/news'
-axios
-
-useEffect
+import ProductCard from '../../components/productCard'
 
 function Main() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const responce = await axios.get(
+          'https://691226ca52a60f10c820ce49.mockapi.io/productData'
+        )
+        setProducts(responce.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProducts()
+  }, [])
   return (
-    <Box>
+    <Box sx={{ px: 31.25 }}>
       <News />
-      <Typography>Товары</Typography>
+      <Typography
+        sx={{ fontSize: 36, fontWeight: 700, color: '#000000', mt: 9 }}
+      >
+        Товары
+      </Typography>
+      <Divider sx={{ mt: 4, mb: 6 }} />
+      <Grid container justifyContent={'center'} spacing={16} pb={21}>
+        {products.map((product) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   )
 }
